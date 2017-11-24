377b716b7742eddc4af661151546d8637d6f5ac2
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2017-11-24 09:20:12 +0100

    packman finit

--- src/model/Movable.java
+++ src/model/Movable.java
@@ -1,9 +1,9 @@
 package model;
 
 public abstract class Movable implements IMovable {
-	private int _x;
-	private int _y;
-	private Boolean _isPackman;
+	protected int _x;
+	protected int _y;
+	protected Boolean _isPackman;
 
 	public void moveUp() {
 		_y += 1;
--- src/model/Packman.java
+++ src/model/Packman.java
@@ -1,8 +1,22 @@
 package model;
 
 public class Packman extends Movable {
+	private static Packman instance = null;
 
-	public Packman(int x, int y, Boolean isPackman) {
+	private Packman(int x, int y) {
+		_x = x;
+		_y = y;
+		_isPackman = true;
+	}
 
+	public static Packman createInstance(int x, int y) {
+		if (instance == null) {
+			instance = new Packman(x, y);
+		}
+		return instance;
+	}
+
+	public static Packman getInstance() {
+		return instance;
 	}
 }
