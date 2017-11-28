267145f08a69ee427330f496c6ba3eeb9338fdad
mmayolini <maxime.mayolini@gmail.com>
2017-11-28 13:14:17 +0100

    move sprites and update view

--- src/view/View.java
+++ src/view/View.java
@@ -14,8 +14,36 @@ public class View {
 		return instance;
 	}
 	
-	public void start(Stage primaryStage) {
-		//TODO
+	public void start(Stage primaryStage/*,JGraph graph*/) {
+		//int nbSommets = graph.getNbSommets();
+		int size = 3; //(int) Math.sqrt(nbSommets);
+		ViewFrame.drawFrame(primaryStage, size, size);
+		/*for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				V v1 = graph.getSommet(i, j);
+				V v2;
+				if((v2 = graph.getSommet(i-1, j)) != NULL && !graph.areAdjacent(v1, v2))
+					ViewFrame.drawWall(i, j, i-1, j, ViewFrame.WALL_COLOR);
+				else if((v2 = graph.getSommet(i+1, j)) != NULL && !graph.areAdjacent(v1, v2))
+					ViewFrame.drawWall(i, j, i+1, j, ViewFrame.WALL_COLOR);
+				else if((v2 = graph.getSommet(i, j-1)) != NULL && !graph.areAdjacent(v1, v2))
+					ViewFrame.drawWall(i, j, i, j-1, ViewFrame.WALL_COLOR);
+				else if((v2 = graph.getSommet(i, j+1)) != NULL && !graph.areAdjacent(v1, v2))
+					ViewFrame.drawWall(i, j, i, j+1, ViewFrame.WALL_COLOR);
+			}
+		}*/
+		ViewFrame.drawWall(1, 0, 1, 1, ViewFrame.WALL_COLOR);
+		ViewFrame.drawWall(2, 0, 2, 1, ViewFrame.WALL_COLOR);
+		ViewFrame.drawWall(0, 2, 1, 2, ViewFrame.WALL_COLOR);
+		ViewFrame.drawWall(1, 1, 2, 1, ViewFrame.WALL_COLOR);
+		ViewFrame.drawPlayer(2, 0);
+		ViewFrame.drawDoor(0, 2);
+		ViewFrame.drawCandy(0, 0);
+		ViewFrame.drawVillain(2, 1);
+	}
+	
+	public void updatePlayer(int x, int y) {
+		ViewFrame.updatePlayer(x, y);
 	}
 
 }
--- src/view/ViewFrame.java
+++ src/view/ViewFrame.java
@@ -1,8 +1,11 @@
 package view;
 
 import javafx.stage.Stage;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
@@ -12,8 +15,14 @@ public class ViewFrame {
 	static final int WALL = 2; // thickness of the walls (in units)
 	static final int CELL = 9; // size of the cells (in units)
 	public static final Paint WALL_COLOR = Color.BURLYWOOD;
-	public static final Paint SCENE_COLOR = Color.BURLYWOOD;
+	public static final Paint SCENE_COLOR = Color.WHITE;
 	private static Group pane;
+	private static Image player = new Image("img/player.png");
+	private static Image door = new Image("img/door_open.png");
+	private static Image villain = new Image("img/bad.png");
+	private static Image candy = new Image("img/candy-1.png");
+	private static ImageView vPlayer = new ImageView(player);
+	private static ImageView vVillain = new ImageView(villain);
 	
 	public static void drawFrame(Stage stage, int nbrX, int nbrY) {
 		pane = new Group();
@@ -55,6 +64,7 @@ public class ViewFrame {
 				pane.getChildren().add(square);
 			}
 		}
+		stage.show();
 	}
 	
 	public static void drawWall(int xs, int ys, int xt, int yt, Paint color) {
@@ -78,4 +88,47 @@ public class ViewFrame {
 			pane.getChildren().add(square);
 		}
 	}
+	
+	public static void drawPlayer(int x, int y) {
+		x = WALL * SPAN + x * (WALL + CELL) * SPAN;
+		y = WALL * SPAN + y * (WALL + CELL) * SPAN;
+		vPlayer.relocate(x, y);
+		pane.getChildren().add(vPlayer);
+	}
+	
+	public static void updatePlayer(int x, int y) {
+		x = WALL * SPAN + x * (WALL + CELL) * SPAN;
+		y = WALL * SPAN + y * (WALL + CELL) * SPAN;
+		vPlayer.relocate(x, y);
+	}
+	
+	public static void drawDoor(int x, int y) {
+		ImageView vDoor = new ImageView(door);
+		x = WALL * SPAN + x * (WALL + CELL) * SPAN;
+		y = WALL * SPAN + y * (WALL + CELL) * SPAN;
+		vDoor.relocate(x, y);
+		pane.getChildren().add(vDoor);
+	}
+	
+	public static void drawVillain(int x, int y) {
+		x = WALL * SPAN + x * (WALL + CELL) * SPAN;
+		y = WALL * SPAN + y * (WALL + CELL) * SPAN;
+		vVillain.relocate(x, y);
+		pane.getChildren().add(vVillain);
+	}
+	
+	public static void updateVillain(int x, int y) {
+		x = WALL * SPAN + x * (WALL + CELL) * SPAN;
+		y = WALL * SPAN + y * (WALL + CELL) * SPAN;
+		vVillain.relocate(x, y);
+	}
+	
+	public static void drawCandy(int x, int y) {
+		ImageView vCandy = new ImageView(candy);
+		x = WALL * SPAN + x * (WALL + CELL) * SPAN;
+		y = 3*WALL * SPAN + y * (WALL + CELL) * SPAN;
+		vCandy.relocate(x, y);
+		pane.getChildren().add(vCandy);
+	}
+	
 }
