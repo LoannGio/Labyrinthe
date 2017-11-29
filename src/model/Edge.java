f64cd55e1ccb71979ef0956e2df7a6f15ebca16c
Didine24 <didine1995@hotmail.com>
2017-11-29 12:19:48 +0100

    Changed Edge Class

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -1,85 +1,31 @@
 package model;
 
-import org.jgrapht.graph.DefaultEdge;
-
-public class Edge extends DefaultEdge implements Comparable<Edge>{
+public class Edge {
 	
-	private Type type;
+	private Vertex a;
+	private Vertex b;
 	
-	public enum Type{
-		OPENED_DOOR,
-		CLOSED_DOOR,
-		CORRIDOR;
-	};
 	
-	public Edge(Type type) {
+	public Edge(Vertex a, Vertex b) {
 		super();
-		this.type = type;
-	}
-
-	//default
+		this.a = a;
+		this.b = b;
+	}	
 	
-	public Edge() {
-		super();
-		this.type = Type.CORRIDOR;
+	public Vertex getA() {
+		return a;
 	}
 	
-	public Vertex getSource() {
-		return (Vertex) super.getSource();
+	public void setA(Vertex a) {
+		this.a = a;
 	}
 	
-	public Vertex getTarget() {
-		return (Vertex) super.getTarget();
+	public Vertex getB() {
+		return b;
 	}
 	
-	public Type getType() {
-		return type;
+	public void setB(Vertex b) {
+		this.b = b;
 	}
 	
-	public void setType(Type type) {
-		this.type = type;
-	}
-	
-	public void closeDoor(Edge edge) {
-		edge.setType(Edge.Type.CLOSED_DOOR);
-	}
-	
-	
-	// FONCTIONS QUI SUIVENT A REPLACER ??
-	
-	public void closeDoorRandom() {
-		Edge edge = graph.randomEdge();
-		closeDoor(edge);
-	}
-	
-	public boolean isWall(Vertex vertex, Directions dir) {
-		Edge edge = graph.getEdge(vertex, dir);
-		return (edge == null);
-	}
-	
-	public boolean isClosed(Vertex vertex, Directions dir) {
-		Edge edge = graph.getEdge(vertex, dir);
-		return (edge == null || (edge.getType()==Edge.Type.CLOSED_DOOR)));
-	}
-	
-	public boolean isOpened(Vertex vertex, Directions dir) {
-		Edge edge = graph.getEdge(vertex, dir);
-		return ((edge!=null)&&((edge.getType()!=Edge.Type.CLOSED_DOOR)));
-	}
-	
-	public boolean isClosedDoor(Vertex vertex, Directions dir) {
-		Edge edge = graph.getEdge(vertex, dir);
-		return (edge!=null && edge.getType()==Edge.Type.CLOSED_DOOR);
-	}
-
-	public boolean isOpenedDoor(Vertex vertex, Directions dir) {
-		Edge edge = graph.getEdge(vertex, dir);
-		return ((edge!=null) && ((edge.getType()==Edge.Type.OPENED_DOOR)));
-	}
-
-	@Override
-	public int compareTo(Edge o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 }
