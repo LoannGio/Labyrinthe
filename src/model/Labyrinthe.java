37c57c7c66bf9de3fb690caac94b041de2d56ea0
Didine24 <didine1995@hotmail.com>
2017-11-29 12:26:55 +0100

    Implemented Labyrinthe Class

--- src/model/Labyrinthe.java
+++ src/model/Labyrinthe.java
@@ -1,83 +1,113 @@
 package model;
 
-import org.jgrapht.Graph;
-
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Queue;
-import java.util.Vector<E>;
+
 
 public class Labyrinthe {
-
-	public void buildRandomPath(Vertex vertex) {
-		//une liste aleatoire des 4 directions
-		Vector<Directions> v = new Vector<Directions>();
-		
-		for(int i = 0; i < 4; ++i)
-			v.add(Directions.values()[i]);
-		Directions directions[] = new Directions[4];
-		for(int i = 0; i < directions.length; ++i) {
-			int index = random.nextInt(v.size());
-			directions[i] = v.get(index);
-			v.remove(index);
-		}
-		
-		//pour chacune de ces directions, on avance en profondeur d abord
-		
-		for(int i=0; i < 4; ++i) {
-			Directions dir = directions[i];
-			if (vertex.inBorders(dir) && Graph.doesntExist(vertex.dir)) {
-				int x = vertex.getX();
-				int y = vertex.getY();
-				int xt = 0, yt = 0;
-				switch(dir) {
-					case NORTH: xt = x; yt = y-1; break;
-					case SOUTH: xt = x; yt = y+1; break;
-					case EAST: xt = x+1; yt = y; break;
-					case WEST: xt = x-1; yt = y; break;
-				}
-				Vertex next = new Vertex(xt, yt, vertex.getNbr()+1);
-				graph.addVertex(next);
-				graph.addEdge(vertex, next);
-				buildRandomPath(next);
-			}
-		}			
+	
+	protected static final int TOP_BORDER = 0;
+	protected static final int DOWN_BORDER = 15;
+	protected static final int LEFT_BORDER = 0;
+	protected static final int RIGHT_BORDER = 15;
+	
+	protected Graph g;
+	protected Packman packman;
+	protected Door Door;
+	
+	public enum direction {
+		North,
+		East,
+		South,
+		West;
 	}
 	
-	public void openDoorRandom() {
-		// On essaie 1000 fois, apres quoi on renonce
-		for(int i=1; i<=1000;++i) {
-			//On choisi un sommet au hasard
-			Vertex vertex = graph.randomVertex();
-			if(vertex!=null) {
-				//On choisi une direction au hasard (on devrait prendre seulement
-				// celles qui correspondent a des murs...)
-				
-				Labyrinth.Directions dir = Directions.values()[random.nextInt(Directions.values().length)];
-				if(isWall(Vertex,dir)) {
-					Vertex vertex2 = graph.getVertexByDir(vertex, dir);
-					if(vertex2!=null) {
-						// On rajoute un saut entre ces sommets
-						graph.addEdge(vertex,vertex2);
-							new Edge(Type.OPENED_DOOR));
-						return;
-					}
+	public Labyrinthe() {
+		g = new Graph();
+		packman = new Packman();
+		Door = new Door();
+	}
+	
+	public Graph getG() {
+		return g;
+	}
+	
+	public Packman getpackman() {
+		return packman;
+	}
+
+	public Door getDoor() {
+		return Door;
+	}
+
+	public int getRIGHT_BORDER() {
+		return RIGHT_BORDER;
+	}
+	
+	public int getDOWN_BORDER() {
+		return DOWN_BORDER;
+	}
+
+	public void buildPath(Vertex v) {
+		List<direction> direct = new ArrayList<>();
+		Collections.addAll(direct, direction.values());
+		Collections.shuffle(direct);
+		
+		for (int i = 0 ; i < direct.size() ; i++) {
+			Vertex v2;
+			direction dir = direct.get(i);
+			if (v.inBorders(dir) && g.vertexDoesntExist(v, dir) && g.edgeDoesntExist(v, dir)) {
+				v2 = g.vertexByDir(v, dir);
+				v2.setNbr(v.getNbr()+1);
+				g.addVertex(v2);
+				g.addEdge(v, v2);
+				System.out.println("edge : "+"("+v.toString()+","+v2.toString()+") "+dir);
+				buildPath(v2);
+			}
+		}
+	}
+	
+	public void opendDoorRandom() {
+		List<direction> direct = new ArrayList<>();
+		Collections.addAll(direct, direction.values());
+		
+		for (int i = 0 ; i < 1000 ; i++) {
+			Vertex v = g.getEqualVertex(g.randomVertex());
+			if (v != null) {
+				Collections.shuffle(direct);
+				direction dir = direct.get(0);
+				if (g.edgeDoesntExist(v,dir)) {
+					Vertex v2 = g.getEqualVertex(g.vertexByDir(v, dir));
+					if (v2 != null) {
+						Edge edge = g.getG().getEdge(v, v2);
+						if (edge == null) {
+							g.addEdge(v, v2);
+							System.out.println("added edge : "+"("+v.toString()+","+v2.toString()+")");
+							return;
+						}
+ 					}
 				}
 			}
 		}
 	}
 	
 	private void calculateManhattanDistance(Vertex source, Vertex target) {
-		Queue<Vertex> fifo = new ArrayDeque<Vertex> ();
+		Queue<Vertex> fifo = new ArrayDeque<Vertex>();
 		target.setNbr(1);
 		fifo.add(target);
 		while(!fifo.isEmpty()) {
 			Vertex actual = fifo.remove();
-			for(Directions dir : Directions.values()) {
-				if(this.isOpened(actual, dir)) {
-					Vertex next = graph.getVertexByDir(actual,dir);
-					if(next.getNbr()==0) {
+			for (direction dir : direction.values()) {
+				if (!this.g.edgeDoesntExist(actual, dir)) {
+					Vertex next = g.getEqualVertex(g.vertexByDir(actual, dir));
+					if (next.getNbr() == 0) {
 						next.setNbr(actual.getNbr()+1);
-						if(next!=source)
+						if (next != source) {
 							fifo.add(next);
+						}
 					}
 				}
 			}
@@ -85,10 +115,12 @@ public class Labyrinthe {
 	}
 	
 	public void launchManhattan(Vertex source, Vertex target) {
-		for(Vertex vertex : graph.vertexSet()) {
-			vertex.setNbr(0);
+		for (Object vertex : g.vertexSet()) {
+			((Vertex) vertex).setNbr(0);
 		}
-		calculateManhattanDistance(source,target);
+		calculateManhattanDistance(source, target);
 	}
-
+	
 }
+
+
