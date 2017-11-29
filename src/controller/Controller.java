9f1791037eecc020dfa92ea9fcf1170ae8d2d4df
Didine24 <didine1995@hotmail.com>
2017-11-29 12:52:49 +0100

    Add Labyrinth in Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,15 +2,23 @@ package controller;
 
 import javafx.stage.Stage;
 import view.View;
+import model.*;
 
 public class Controller {
 
 	private static Controller instance = new Controller();
 	private View view;
+	private Labyrinthe laby;
 
 	private Controller() {
 		// model = Model.getInstance();
 		view = View.getInstance();
+		laby = new Labyrinthe();
+		laby.getDoor().startPosition();
+		Vertex v = laby.getDoor().getPosition();
+		laby.getG().addVertex(v);
+		laby.buildPath(v);
+		laby.getPackman().startPosition(laby, laby.getG().getEqualVertex(v));
 	}
 
 	public static Controller getInstance() {
