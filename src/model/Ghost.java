483ec1117c3ad130a3da536f31906201970b98dc
Didine24 <didine1995@hotmail.com>
2017-11-29 12:24:13 +0100

    Update Ghost Class

--- src/model/Ghost.java
+++ src/model/Ghost.java
@@ -1,12 +1,25 @@
 package model;
 
-public class Ghost extends Movable{
+import model.Labyrinthe.direction;
+
+public class Ghost extends Movable {
 	
-	public Ghost(int x, int y) {
-		_x = x;
-		_y = y;
-		_isPackman = false;	
+	public void	move(Labyrinthe labyrinthe) {
+		Vertex vertex = this.getRealPosition(labyrinthe.getG());
+		for (direction dir : direction.values()) {
+			Vertex next = labyrinthe.getG().vertexByDir(vertex, dir);
+			if (labyrinthe.getG().containsEdge(vertex, next)
+			&& (next.getNbr() == vertex.getNbr()-1) && next != null) {
+				this.move(labyrinthe, dir);
+			}
+		}
 	}
-	
-	
+
+	@Override
+	void startPosition(Labyrinthe labyrinthe, Vertex niceGuyPosition) {
+		
+		
+	}
+
 }
+
