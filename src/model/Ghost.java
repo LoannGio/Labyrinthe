836e64dcbfe5691474e0dafb787783aa37536612
nmouhoub <nmouhoub@derek.emi.u-bordeaux.fr>
2017-12-11 15:33:47 +0100

    Move Ghost Intelligent

--- src/model/Ghost.java
+++ src/model/Ghost.java
@@ -4,16 +4,30 @@ public class Ghost extends Movable {
 
 	static final int minDist = 10;
 	
+	
+	
+	
 	public void move(Labyrinthe labyrinthe) {
 		Vertex vertex = this.getRealPosition(labyrinthe.getG());
+		int min = 1000; 
+		direction direc = null;
 		for (direction dir : direction.values()) {
-			Vertex next = labyrinthe.getG().vertexByDir(vertex, dir);
+			Vertex next = labyrinthe.getG().getEqualVertex(labyrinthe.getG().vertexByDir(vertex, dir));
 			if (labyrinthe.getG().containsEdge(vertex, next) && (next.getNbr() == vertex.getNbr() - 1)
-					&& next != null) {
-				this.move(labyrinthe, dir);
+				&& next != null){
+				        labyrinthe.launchManhattan(vertex, labyrinthe.getPackman().position);
+				        if(vertex.getNbr()<min) {
+				        	min = vertex.getNbr();
+				        	direc = dir;
+				        }
 			}
 		}
+		if(direc != null)
+			this.move(labyrinthe, direc);
 	}
+
+	
+
 
 	@Override
 	public void startPosition(Labyrinthe labyrinthe, Vertex niceGuyPosition) {
